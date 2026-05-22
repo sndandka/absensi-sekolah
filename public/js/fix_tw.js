b8b4fcbaa21fceffcd41b51019db01ccafcee07b
sandi andika putra <sandiargum@gmail.com>
2026-05-22 12:55:07 +0700

    Delete public directory

--- public/js/fix_tw.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-
-// replace <div class="tw"> with <div class="tw resp-table">
-html = html.replace(/<div class="tw">/g, '<div class="tw resp-table">');
-
-fs.writeFileSync('index.html', html);
-console.log('Done!');
